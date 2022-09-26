be99c3eb1d3b9e2e1f892dd8598551f0ac343d02
nirbhay <nirbhayk7281@gmail.com>
2022-09-26 17:00:20 +0530

    passing state data across components

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -1,15 +1,15 @@
-import React, { useState } from 'react';
- 
-import Card from '../UI/Card';
-import './IngredientForm.css';
+import React, { useState } from "react";
 
-const IngredientForm = React.memo(props => {
+import Card from "../UI/Card";
+import "./IngredientForm.css";
 
-  const [inputState, setInputState] = useState({ title: '', amount: '' });
+const IngredientForm = React.memo((props) => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmound, setEnteredAmount] = useState("");
 
-  const submitHandler = event => {
+  const submitHandler = (event) => {
     event.preventDefault();
-    // ...
+    props.onAddIngredient({ title: enteredTitle, amount: enteredAmound });
   };
 
   return (
@@ -21,13 +21,9 @@ const IngredientForm = React.memo(props => {
             <input
               type="text"
               id="title"
-              value={inputState.title}
-              onChange={event => {
-                const newTitle = event.target.value;
-                setInputState(prevInputState => ({
-                  title: newTitle,
-                  amount: prevInputState.amount
-                }));
+              value={enteredTitle.title}
+              onChange={(event) => {
+                setEnteredTitle(event.target.value);
               }}
             />
           </div>
@@ -36,13 +32,9 @@ const IngredientForm = React.memo(props => {
             <input
               type="number"
               id="amount"
-              value={inputState.amount}
-              onChange={event => {
-                const newAmount = event.target.value;
-                setInputState(prevInputState => ({
-                  amount: newAmount,
-                  title: prevInputState.title
-                }));
+              value={enteredAmound.amount}
+              onChange={(event) => {
+                setEnteredAmount(event.target.value);
               }}
             />
           </div>
--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import IngredientForm from './IngredientForm';
-import Search from './Search';
+import IngredientForm from "./IngredientForm";
+import IngredientList from "./IngredientList";
+import Search from "./Search";
 
-function Ingredients() {
+const Ingredients = () => {
+  const [userIngredients, setUserIngredients] = useState([]);
+
+  const addIngredientHandler = (ingredient) => {
+    setUserIngredients((prevIngredients) => [
+      ...prevIngredients,
+      { id: Math.random().toString(), ...ingredient },
+    ]);
+  };
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
         <Search />
+        <IngredientList ingredients={userIngredients} onRemoveItem={()=>{}}/>
+
         {/* Need to add list here! */}
       </section>
     </div>
-  ); 
-}
+  );
+};
 
 export default Ingredients;
-  
