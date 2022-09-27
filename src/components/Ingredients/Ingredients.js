9a3e5011949027d08f9f3fe64b19f01be3a94c13
nirbhay <nirbhayk7281@gmail.com>
2022-09-27 14:32:03 +0530

    loading data

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
@@ -6,7 +6,27 @@ import Search from "./Search";
 
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
+    /*loading data */
+  useEffect(() => {
+  fetch(
+    "https://react-hooks-update-889d9-default-rtdb.firebaseio.com/ingredients.json"
+  )
+    .then((res) => res.json())
+    .then((resData) => {
+      const loadedIngredients = [];
+      for(const key in resData){
+        loadedIngredients.push({
+          id:key,
+          title:resData[key].title,
+          amount:resData[key].amount
+        });
+      }
+      setUserIngredients(loadedIngredients);
+    }) ;
+  },[])
+    /*End loading data */
 
+  /* fetching data*/  
   const addIngredientHandler = (ingredient) => {
     fetch(
       "https://react-hooks-update-889d9-default-rtdb.firebaseio.com/ingredients.json",
@@ -15,18 +35,18 @@ const Ingredients = () => {
         body: JSON.stringify(ingredient),
         headers: { "Content-Type": "application/json" },
       }
-    ).then((res) => {
-      return res.json();
-    }).then(resData=>{
-      setUserIngredients((prevIngredients) => [
-        ...prevIngredients,
-        { id:resData.name, ...ingredient },
-      ]);
-    });
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((resData) => {
+        setUserIngredients((prevIngredients) => [
+          ...prevIngredients,
+          { id: resData.name, ...ingredient },
+        ]);
+      });
   };
-
-  // https://react-hooks-update-889d9-default-rtdb.firebaseio.com
-
+    /*End feching data */
   return (
     <div className="App">
       <IngredientForm onAddIngredient={addIngredientHandler} />
